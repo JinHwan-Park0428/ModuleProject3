fe148ba6f500c21ac17e13d1fc48cf4b2fbeaea1
JinHwan-Park0428 <76460529+JinHwan-Park0428@users.noreply.github.com>
2021-03-16 15:44:47 +0900

    Update visualize.py

--- MDproject3/visualize.py
+++ MDproject3/visualize.py
@@ -1,61 +1,87 @@
 import pymysql
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from pandas import DataFrame
 from datetime import datetime
 
 plt.style.use('ggplot')
-connectdb = pymysql.connect(host='localhost', user='root', password='', port=3306, db='mydb', charset='utf8')
-cursor = connectdb.cursor()
+
+class Mdproject3:
+    def __init__(self):
+        self.connectdb = pymysql.connect(host='jinhwan-instance.cn73pxf4sggf.us-west-2.rds.amazonaws.com', user='admin', password='1q2w3e4r5t', port=3306, db='mydb',
+                                         charset='utf8')
+        self.cursor = self.connectdb.cursor()
+        # self.barchart()
+        self.per_5m_linechart()
+        self.cursor.close()
+        self.connectdb.close()
+
+    def newtime(self, x):
+        return x.strftime('%Y-%m-%d %H:%M:%S')
+
+    def time_minute(self, x):
+        return x.split(' ')[1].split(".")[0].split(":")[1]
+
+    def time_hour(self, x):
+        return x.split(' ')[1].split(".")[0].split(":")[0]
+
+    def save_data(self):
+        show_db = '''SELECT * FROM my_project_stock'''
+        self.cursor.execute(show_db)
+        data = self.cursor.fetchall()
+        pddata = pd.DataFrame(data, columns=["quantity", "price", "days_range", "title", "open_price", "ratio", "search_time"])
+        pddata["search_time"] = pddata["search_time"].map(lambda x: self.newtime(x))
+        pddata["time_hour"] = pddata["search_time"].map(lambda x: self.time_hour(x))
+        pddata["time_minute"] = pddata["search_time"].map(lambda x: self.time_minute(x))
+
+        # print(pddata.sort_values("search_time", ascending=True))
+        return pddata
+
+    def barchart(self):
+        self.save_data()
+        new_time_list = []
+        for time in self.search_time_list[:5]:
+            new_time_list.append(time.minute)
+        price_index = range(len(self.price_list[:5]))
+
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 2, 1)
+        ax1.bar(price_index, self.price_list[:5], align='center', color='darkblue')
+        ax1.xaxis.set_ticks_position('bottom')
+        ax1.yaxis.set_ticks_position('left')
+        plt.xticks(price_index, new_time_list, rotation=0, fontsize='large')
+        plt.ylim(58500, 59000)
+        plt.yticks(np.arange(58500, 59100, 100))
+
+        plt.xlabel('minutes')
+        plt.ylabel('Price')
+        plt.title('Data per minute for {}th BTC Price'.format(self.search_time_list[0].day))
+        plt.savefig('bar_plot.png', dpi=400, bbox_inches='tight')
+        plt.show()
+
+    def per_5m_linechart(self):
+        df = self.save_data()
+        hours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+
+        for i in hours:
+            ndata = df[df["time_hour"] == i]
+            if len(ndata) != 0:
+                fig = plt.figure()
+                ax1 = fig.add_subplot(1, 1, 1)
+                ax1.plot(ndata["price"], marker=r'o', color=u'blue', linestyle='-', label='Blue Solid')
+                plt.xticks(range(len(ndata["time_minute"])), ndata["time_minute"], rotation=0, fontsize="large")
+                ax1.xaxis.set_ticks_position('bottom')
+                ax1.yaxis.set_ticks_position('left')
+
+                ax1.set_title("Data per 5min for {}O'clock BTC Price".format(i))
+                plt.xlabel('minutes')
+                plt.ylabel('price')
+                # plt.legend(loc='best')
+
+                plt.savefig('line_plot' +i+ '.png', dpi=400, bbox_inches='tight')
+                plt.show()
 
 
-def Look_db():
-    quantity_list = []
-    price_list = []
-    days_max_list = []
-    days_min_list = []
-    title_list = []
-    search_time_list = []
-    new_time_list = []
-    show_db = '''SELECT * FROM my_project_md3stock'''
-    cursor.execute(show_db)
-    data = cursor.fetchone()
-    if data != None:
-        while data:
-            print("\nQuantity: {0}, Price: {1}, Days_range: {2}, Title: {3}, search_time: {4}".format(data[0],
-                                                                                                      data[1],
-                                                                                                      data[2],
-                                                                                                      data[3],
-                                                                                                      data[4]))
-            quantity_list.append(data[0])
-            price_list.append(data[1])
-            days_max_list.append(data[2].strip().split(" - ")[1])
-            days_min_list.append(data[2].strip().split(" - ")[0])
-            title_list.append(data[3])
-            search_time_list.append(data[4])
-            data = cursor.fetchone()
-    else:
-        print("\n데이터가 존재하지 않습니다.")
-
-    # print(search_time_list)
-    price_index = range(len(price_list[:5]))
-    # for i in search_time_list[:5]:
-    #     new_time_list.append(datetime.strptime(i, '%Y-%m-%d %H:%M:%S'))
-
-    fig = plt.figure()
-    ax1 = fig.add_subplot(1, 2, 1)
-    ax1.bar(price_index, price_list[:5], align='center', color='darkblue')
-    ax1.xaxis.set_ticks_position('bottom')
-    ax1.yaxis.set_ticks_position('left')
-    plt.xticks(price_index, new_time_list, rotation=90, fontsize='small')
-    plt.ylim(58500, 59000)
-    plt.yticks(np.arange(58500, 59100, 100))
-
-    plt.xlabel('datetime')
-    plt.ylabel('Quantity')
-    plt.title('BTC Graph')
-
-    # plt.savefig('bar_plot.png', dpi=400, bbox_inches='tight')
-    plt.show()
-
-
-Look_db()
+if __name__ == '__main__':
+    Mdproject3()
