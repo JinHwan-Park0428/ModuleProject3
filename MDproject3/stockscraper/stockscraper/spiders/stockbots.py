7ce3f3a8bc284df0997e6bec456b5028bf329e11
leeheesuk <43261413+leeheesuk@users.noreply.github.com>
2021-03-16 10:30:54 +0900

    Update stockbots.py

--- MDproject3/stockscraper/stockscraper/spiders/stockbots.py
+++ MDproject3/stockscraper/stockscraper/spiders/stockbots.py
@@ -22,3 +22,23 @@ class StockbotsSpider(scrapy.Spider):
             item['title'] = row[3]                                                
 
             yield item
+            
+#     open_price = 시작가 , ratio = 시작가와 현재가의 차이 ( 가격변동률 )
+#     def parse(self, response):
+#         Quantity = response.xpath('//*[@id="quote-summary"]/div[2]/table/tbody/tr[4]/td[2]/span/text()').extract()
+#         Price = response.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[1]/text()').extract()        
+#         days_range = response.xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[3]/td[2]/text()').extract()
+#         title = response.xpath('//*[@id="quote-header-info"]/div[2]/div[1]/div[1]/h1/text()').extract()
+#         ratio = response.xpath('//*[@id="quote-header-info"]/div[3]/div[1]/div/span[2]/text()').extract()
+#         open_price = response.xpath('//*[@id="quote-summary"]/div[1]/table/tbody/tr[2]/td[2]/span/text()').extract()
+
+#         for row in zip(Quantity, Price, days_range,  title,ratio, open_price):
+#             item = StockscraperItem()
+#             item['Quantity'] = row[0]
+#             item['Price'] = row[1]
+#             item['days_range'] = row[2]
+#             item['title'] = row[3]       
+#             item['ratio'] = row[4]    
+#             item['open_price'] = row[5]                                     
+
+#             yield item
